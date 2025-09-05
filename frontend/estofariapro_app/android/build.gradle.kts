684bf23032b380e13492a4434dae59755870ae02
Ivam Pereira <comsudlg@gmail.com>
2025-09-05 17:41:34 -0300

    feat: configuração completa do Firebase + Android

--- frontend/estofariapro_app/android/build.gradle.kts
+++ frontend/estofariapro_app/android/build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +13,5 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+dependencies {
 }
